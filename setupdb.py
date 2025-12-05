69c051ba072b402c6324c4ebb4fe31622d0d610a
Adam Muqhriz <adamswork2611@gmail.com>
2025-12-05 11:26:14 +0800

    Restart deployment

--- setupdb.py
+++
@@ -1,6 +0,0 @@
-from app import db, app
-
-print("Running db.create_all() for database initialization...")
-with app.app_context():
-    db.create_all()
-    print("Database tables successfully created.")
